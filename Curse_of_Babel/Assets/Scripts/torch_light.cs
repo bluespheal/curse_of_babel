b4eb12a132c38254b1ce2d654b153826b767febe
BaconWarrior <61850017+BaconWarrior@users.noreply.github.com>
2020-04-24 23:49:37 -0500

    Antorchas y candelabros
    Se agregaron las particulas  y la iluminacion de las antorchas y faroles. Se puede controlar la intencidad de la iluminacion sin necesidad de modificar el script

---
+++ Curse_of_Babel/Assets/Scripts/torch_light.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class torch_light : MonoBehaviour
+{
+    Light fireLight;
+    float light_Int;
+    public float minInt = 0.5f, maxInt = 1f;
+    void Start()
+    {
+        fireLight = GetComponent<Light>();
+    }
+
+    void Update()
+    {
+        light_Int = Random.Range(minInt, maxInt);
+        fireLight.intensity = light_Int;
+    }
+}
