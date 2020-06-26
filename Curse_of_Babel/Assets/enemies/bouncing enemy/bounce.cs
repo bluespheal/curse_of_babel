767c432d0cde368a8b80c3d3fbaef7f8b101b038
jose maria beristain <dedocebabotas@hotmail.com>
2020-06-25 19:00:53 -0500

    fix

--- Curse_of_Babel/Assets/enemies/bouncing enemy/bounce.cs
+++ Curse_of_Babel/Assets/enemies/bouncing enemy/bounce.cs
@@ -8,15 +8,10 @@ public class bounce : enemy
     private float startY;
     private Rigidbody rigi;
 
-    public float bounceLenght;
-    public int bounces;
-    public int bouncesLeft;
-
     void Awake()
     {
         rigi = transform.GetComponent<Rigidbody>();
         startY = transform.position.y;
-        bouncesLeft = bounces;
     }
 
     // Update is called once per frame
@@ -24,26 +19,8 @@ public class bounce : enemy
     {
         if (!stop)
         {
-            if (bounceLenght != 0)
-            {
-                rigi.velocity = new Vector3(bounceLenght, rigi.velocity.y, rigi.velocity.z);
-            }
             if (transform.position.y >= startY) {
                 rigi.velocity = new Vector3(rigi.velocity.x,0,0);
-            }
-        }
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (bounceLenght != 0) {
-            if (collision.transform.CompareTag("platform")) {
-                if (bouncesLeft <= 0)
-                {
-                    bounceLenght = -bounceLenght;
-                    bouncesLeft = bounces;
-                }
-                bouncesLeft -= 1;
             }
         }
     }
